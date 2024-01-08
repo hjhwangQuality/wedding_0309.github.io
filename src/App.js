1f497d27b9e9df142389dad783b432f65efd9e32
hjhwang <44138573+hjhwangQuality@users.noreply.github.com>
2024-01-08 21:36:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import cover_image from './image/PV3_3547.JPG'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <div class="TextBox"> <h3>2024.03.09</h3>
+        </div>
+           
+      <img class='cover' src={cover_image} alt=""/>
+      
+      <div class="TextBox_name"> <h4>황희정 💗 오제형</h4></div>
+      <div class="content_text"><p>2024년 3월 9일 토요일 16시<br /> 프레스센터 20층 국제회의장 (플라자웨딩)</p></div>
+      
+      
     </div>
-  );
+    
+  )
 }
 
 export default App;
